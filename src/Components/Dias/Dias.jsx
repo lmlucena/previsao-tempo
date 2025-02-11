7dcfd8db67f029ab16281186c73dfd6f77aa6b90
lmlucena <lucas.lmlucena@gmail.com>
2025-02-11 09:57:31 -0300

    ajustes nos blocos

--- src/Components/Dias/Dias.jsx
+++ src/Components/Dias/Dias.jsx
@@ -9,7 +9,7 @@ function Dias ({ weatherDays }) {
             <div className="flex p-3 justify-evenly mt-2.5 flex-wrap gap-2.5">
 
                 {/* DIA 1*/}    
-                <div className="flex flex-col items-center p-4 rounded-md bg-white">
+                <div className="flex flex-col items-center p-3.5 rounded-md bg-white">
                     <p className="text-2xl font-bold">Dia {new Date(weatherDays.list[7].dt_txt + " UTC").toLocaleDateString("pt-BR", {day: 'numeric', month: "short"})}</p>
                     <img className="" src={`http://openweathermap.org/img/wn/${weatherDays.list[7].weather[0].icon}.png`} alt="icone" />
                     <p>{Math.round(weatherDays.list[7].main.temp)}ºC</p>
@@ -17,7 +17,7 @@ function Dias ({ weatherDays }) {
                 </div>
 
                 {/* DIA 2 */}
-                <div className="flex flex-col items-center p-4 rounded-md bg-white">
+                <div className="flex flex-col items-center p-3.5 rounded-md bg-white">
                     <p className="text-2xl font-bold">Dia {new Date(weatherDays.list[15].dt_txt + " UTC").toLocaleDateString("pt-BR", {day: 'numeric', month: "short"})}</p>
                     <img className="" src={`http://openweathermap.org/img/wn/${weatherDays.list[15].weather[0].icon}.png`} alt="icone" />
                     <p>{Math.round(weatherDays.list[15].main.temp)}ºC</p>
@@ -25,7 +25,7 @@ function Dias ({ weatherDays }) {
                 </div>   
 
                 {/* DIA 3 */}
-                <div className="flex flex-col items-center p-4 rounded-md bg-white">
+                <div className="flex flex-col items-center p-3.5 rounded-md bg-white">
                     <p className="text-2xl font-bold">Dia {new Date(weatherDays.list[23].dt_txt + " UTC").toLocaleDateString("pt-BR", {day: 'numeric', month: "short"})}</p>
                     <img className="" src={`http://openweathermap.org/img/wn/${weatherDays.list[23].weather[0].icon}.png`} alt="icone" />
                     <p>{Math.round(weatherDays.list[23].main.temp)}ºC</p>
@@ -33,7 +33,7 @@ function Dias ({ weatherDays }) {
                 </div>   
 
                 {/* DIA 4 */}
-                <div className="flex flex-col items-center p-4 rounded-md bg-white">
+                <div className="flex flex-col items-center p-3.5 rounded-md bg-white">
                     <p className="text-2xl font-bold">Dia {new Date(weatherDays.list[31].dt_txt + " UTC").toLocaleDateString("pt-BR", {day: 'numeric', month: "short"})}</p>
                     <img className="" src={`http://openweathermap.org/img/wn/${weatherDays.list[31].weather[0].icon}.png`} alt="icone" />
                     <p>{Math.round(weatherDays.list[31].main.temp)}ºC</p>
@@ -41,7 +41,7 @@ function Dias ({ weatherDays }) {
                 </div>   
 
                 {/* DIA 5 */}
-                <div className="flex flex-col items-center p-4 rounded-md bg-white">
+                <div className="flex flex-col items-center p-3.5 rounded-md bg-white">
                     <p className="text-2xl font-bold font-bold">Dia {new Date(weatherDays.list[39].dt_txt + " UTC").toLocaleDateString("pt-BR", {day: 'numeric', month: "short"})}</p>
                     <img className="" src={`http://openweathermap.org/img/wn/${weatherDays.list[39].weather[0].icon}.png`} alt="icone" />
                     <p>{Math.round(weatherDays.list[39].main.temp)}ºC</p>
